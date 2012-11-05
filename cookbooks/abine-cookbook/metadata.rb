56a2b75109ee94cb5aafe600e1b261a9abba306d
Ryan Mitchell <Ryan.E.Mitchell@gmail.com>
2012-11-05 16:13:45 -0500

    Added my abine-cookbook

---
+++ cookbooks/abine-cookbook/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Abine Inc."
+maintainer_email "ryan@getabine.com"
+license          "All rights reserved"
+description      "Installs/Configures abine-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+recipe "my-first-cookbook::helloworld", "My first recipe, prints Hello World to the RightScale dashboard"
